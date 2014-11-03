ef0621ac04c2cfb37413d809632f336bda613666
hillmandj <hillmandj@gmail.com>
2014-11-03 00:19:10 -0500

    Installed basic gems, set up first version of application wide layout and styles

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dansite::Application.config.secret_key_base = '35bbaf6e11752af7a9d8e9f079da6fbdb8c10d7476b516e41322a9d0bdb3ae7147f6584c83ea4f1be651c49e19c06c1559d0e6f11f5373b7399f1cf6f577b05b'
