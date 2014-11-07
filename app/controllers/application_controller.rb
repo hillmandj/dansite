0e2700560ee119d796b73c2b5aa02a7baf5c7e4e
hillmandj <hillmandj@gmail.com>
2014-11-07 01:16:33 -0500

    some css work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :set_google_fonts
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+  
+  def set_google_fonts
+    @fonts = ['Droid Serif', 'Ubuntu', 'Roboto', 'Open Sans', 'Rokkitt', 'Inconsolata', 'Oswald']
+  end
 end
