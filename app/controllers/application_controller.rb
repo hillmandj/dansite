aa27e2b647baddfec89ae262b0f4c4d498e4e900
hillmandj <hillmandj@gmail.com>
2015-01-12 22:40:25 -0500

    removed before_action in application controller and hard-coded google font array

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :set_google_fonts
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   private
   
-  def set_google_fonts
-    @fonts = ['Droid Serif', 'Dosis', 'Roboto', 'Open Sans', 'Rokkitt', 'Inconsolata', 'Raleway', 'Lato']
-  end
-
   def after_sign_in_path_for(resource)
     posts_path
   end
