7e3502db3e43b391eebd32ce1eccc6c0dfeab49d
hillmandj <hillmandj@gmail.com>
2015-01-12 21:54:36 -0500

    added media queries, and changed posts route path

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Dansite::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :posts
+  resources :posts, :path => 'blog'
 
   # You can have the root of your site routed with "root"
   root 'pages#about'
