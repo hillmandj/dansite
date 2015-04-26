256285eb2f3462ff1d4894272c1ce898c7ededfc
hillmandj <hillmandj@gmail.com>
2015-04-26 16:46:44 -0400

    changed database cleaner configuration

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -2,11 +2,16 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   
   config.before(:suite) do
-    begin
-      DatabaseCleaner.start
-      FactoryGirl.lint
-    ensure
-      DatabaseCleaner.clean
-    end
+    FactoryGirl.lint
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each)  do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
   end
 end
