b6c142b9fa41a566c47fb059eb9ed7946f0b1b10
hillmandj <hillmandj@gmail.com>
2018-04-12 18:30:18 -0400

    change about page, and add ruby version

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,25 @@
 module PagesHelper
+  def altru_link
+    link_to('Altru', 'https://www.altrulabs.com', target: '_blank')
+  end
+
+  def casper_link
+    link_to('Casper', 'https://www.casper.com', target: '_blank')
+  end
+
+  def handy_link
+    link_to('Handy', 'https://www.handy.com', target: '_blank')
+  end
+
+  def publicstuff_link
+    link_to('PublicStuff', 'https://www.publicstuff.com', target: '_blank')
+  end
+
+  def chloe_link
+    link_to('Chloe + Isabel', 'https://www.chloeandisabel.com', target: '_blank')
+  end
+
+  def oberlin_link
+    link_to('Oberlin College', 'https://www.oberlin.edu', target: '_blank')
+  end
 end
